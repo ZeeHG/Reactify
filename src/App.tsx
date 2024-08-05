4ac584f48fc9e41ea23c649696f1ef1f9b4532dd
mousemen <778992212@qq.com>
2024-08-05 17:10:24 +0800

    feat:button complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
+import Button from "./components/Button/button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
