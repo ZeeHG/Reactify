40dae06fbfc44e0384005397c970b514cf357883
ZeeG <778992212@qq.com>
2024-08-08 16:20:58 +0800

    feat:menuitem

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,23 +1,43 @@
-import React from 'react'
+import React, { useState, createContext } from 'react'
 import classNames from 'classnames'
 type MenuMode = 'horizontal' | 'vertical'
+type SelectCallback = (selectedIndex: number) => void
 export interface MenuProps {
   defaultIndex?: number
   className?: string
   mode?: MenuMode
   style?: React.CSSProperties
   children: React.ReactNode
-  OnSelect?: (selectedIndex: number) => void
+  onSelect?: SelectCallback
 }
 
+interface IMenuContext {
+  index?: number
+  onSelect?: SelectCallback
+}
+export const MenuContext = createContext<IMenuContext>({ index: 0 })
 const Menu: React.FC<MenuProps> = (props) => {
-  const { className, mode, style, children, defaultIndex } = props
+  const { className, mode, style, children, defaultIndex, onSelect } = props
+  const [currentActive, setActive] = useState(defaultIndex)
+  const handleClick = (index: number) => {
+    setActive(index)
+    if (onSelect) {
+      onSelect(index)
+    }
+  }
+  const passedContext: IMenuContext = {
+    index: currentActive ? currentActive : 0,
+    onSelect: handleClick,
+  }
+
   const classes = classNames('menu', className, {
     'menu-vertical': mode === 'vertical',
   })
   return (
     <ul className={classes} style={style}>
-      {children}
+      <MenuContext.Provider value={passedContext}>
+        {children}
+      </MenuContext.Provider>
     </ul>
   )
 }
--- src/components/Menu/menuItem.tsx
+++ src/components/Menu/menuItem.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import classNames from 'classnames'
+import { MenuContext } from './menu'
 export interface MenuItemProps {
-  index?: number
+  index: number
   disabled?: boolean
   className?: string
   children: React.ReactNode
@@ -10,10 +11,17 @@ export interface MenuItemProps {
 
 const MenuItem: React.FC<MenuItemProps> = (props) => {
   const { index, disabled, className, style, children } = props
+  const context = useContext(MenuContext)
   const classes = classNames('menu-item', classNames, {
     'is-disabled': disabled,
+    'is-active': context.index === index,
   })
-  return <li className={classes} style={style}></li>
+  const handleClick = () => {
+    if (context.onSelect && !disabled) {
+      context.onSelect(index)
+    }
+  }
+  return <li className={classes} style={style} onClick={handleClick}></li>
 }
 
 export default MenuItem
